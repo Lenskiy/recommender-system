2993aa98dd554a068eeea8aebf2986ec3fe0c432
Lenskiy <a.a.lensky@gmail.com>
2016-01-25 21:14:48 +0900

    updateed

--- estimateCondititonalPrBernoulli.m
+++ estimateCondititonalPrBernoulli.m
@@ -13,10 +13,10 @@ function Pr_ItemInCategory = estimateCondititonalPrBernoulli(Pr_UratedC, R)
         for c = 1:Ncategories
             Pr_UratedC_temp = Pr_UratedC(:,:,r);
             %ind = find(G(:,c) ~= 0);
-            ind = 1:Nitems;
-            for i = 1:length(ind)%Nitems
+            %ind = 1:Nitems;
+            for i = 1:Nitems
                 %Pr_ItemInCategory_temp(ind(i)) =  prod(R_temp(:,ind(i)) .* Pr_UratedC_temp(:,c) + (1 - R_temp(:,ind(i))) .* (1 - Pr_UratedC_temp(:,c)));
-                Pr_ItemInCategory_temp(ind(i)) =  sum(log(R_temp(:,ind(i)) .* Pr_UratedC_temp(:,c) + (1 - R_temp(:,ind(i))) .* (1 - Pr_UratedC_temp(:,c))));
+                Pr_ItemInCategory_temp(i) =  sum(log(R_temp(:,i) .* Pr_UratedC_temp(:,c) + (1 - R_temp(:,i)) .* (1 - Pr_UratedC_temp(:,c))));
             end
             Pr_ItemInCategory(:, c, r) = Pr_ItemInCategory_temp;
         end   
